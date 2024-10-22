70b2e60050881a1d3b341995cb18cf269f5cb10e
jms-guy <guyjamie92@gmail.com>
2024-10-22 14:00:39 -0400

    Basics

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextType, TextNode
+
+def main():
+    test_node = TextNode("Testing...", TextType.ITALIC, "www.getsome.com")
+    print(test_node)
+
+
+
+if __name__ == "__main__":
+    main()
